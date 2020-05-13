07754f522beac9c5a3050e4c656f3ce83050c7f1
vanessa massiot <vanessa.massiot@free.fr>
2020-05-13 15:25:41 +0200

    some seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,67 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+puts "cleaning database ...."
+Flat.destroy_all
+
+puts "creating flats ..."
+
+flat1= {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url:"https://source.unsplash.com/featured/?{HOME}{FLAT}"
+}
+
+flat2= {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url:"https://source.unsplash.com/featured/?{HOUSE}"
+}
+
+flat3= {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url:"https://source.unsplash.com/featured/?{HOME}{DECO}"
+}
+
+flat4= {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url:"https://source.unsplash.com/featured/?{HOUSE}"
+}
+
+flat5= {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url:"https://source.unsplash.com/featured/?{HOME}{BUILDING}"
+}
+
+flat6= {
+  name: 'Light & Spacious Garden Flat London',
+  address: '10 Clifton Gardens London W9 1DT',
+  description: 'A lovely summer feel for this spacious garden flat. Two double bedrooms, open plan living area, large kitchen and a beautiful conservatory',
+  price_per_night: 75,
+  number_of_guests: 3,
+  picture_url:"https://source.unsplash.com/featured/?{HOUSE}"
+}
+
+[flat1, flat2, flat3, flat4, flat5, flat6].each do |attributes|
+	flat = Flat.create!(attributes)
+	puts "created #{flat.name}"
+end
